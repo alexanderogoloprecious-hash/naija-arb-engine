fcd04df029ad04f5fd1af6e0e87e975b1010a13a
alexanderogoloprecious-hash <alexanderogoloprecious@gmail.com>
2026-09-10 17:00:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import os
+import sys
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class HealthCheckHandler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(b"OK")
+
+  def do_HEAD(self):
+    self.send_response(200)
+    self.end_headers()
+
+
+def start_health_server():
+  port = int(os.environ.get("PORT", 10000))
+  server = HTTPServer(("0.0.0.0", port), HealthCheckHandler)
+  server.serve_forever()
+
+
+threading.Thread(target=start_health_server, daemon=True).start()
+import os
 import threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
